f2c09bf06c8382444f7a6a8ae4c5b46580fe73bc
Mariusz Bugla <mariusz.bugla@mtime.it>
2022-11-08 16:01:49 +0100

    Add project files.

---
+++ HRdata/Program.cs
@@ -0,0 +1,22 @@
+﻿string name="";
+string last_name="";
+int age = 0;
+char sex = ' ';
+string national_ID = "";
+string employee_ID = "";
+
+
+
+Console.WriteLine("Insert employe data:");
+Console.Write("Name:");
+name = Console.ReadLine();
+Console.Write("Last name:");
+last_name = Console.ReadLine();
+Console.Write("Sex:(F/M/NB)");
+sex = Console.ReadLine()[0];
+Console.Write("National ID:");
+national_ID = Console.ReadLine();
+Console.Write("Employee ID:");
+employee_ID = Console.ReadLine();
+
+
